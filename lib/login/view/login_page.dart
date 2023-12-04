31fc7cb38935482a0b2b501d62dbefa6d179f59b
Aidaiym <omurbekovnaaidaiym@gmail.com>
2023-12-04 11:06:03 -0800

    add login

---
+++ lib/login/view/login_page.dart
@@ -0,0 +1,25 @@
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kopuro/core/repos/authentication_repository/authentication_repository.dart';
+import 'package:kopuro/modules/login/logic/login_cubit.dart';
+import 'package:kopuro/modules/login/view/login_form.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  static Page<void> page() => const MaterialPage<void>(child: LoginPage());
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: BlocProvider(
+          create: (_) => LoginCubit(context.read<AuthenticationRepository>()),
+          child: const LoginForm(),
+        ),
+      ),
+    );
+  }
+}
